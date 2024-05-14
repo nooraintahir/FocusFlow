e1178eb8bd4279c57be336f2d25cd67856042287
MariaAhmed01 <mariaahmed2413@gmail.com>
2024-05-14 08:24:00 +0500

    authentication and sqlite

---
+++ app/src/main/java/com/smd/project/DBHelper.kt
@@ -0,0 +1,4 @@
+package com.smd.project
+
+class DBHelper {
+}
---
+++ app/src/main/java/com/smd/project/notifications.kt
@@ -0,0 +1,11 @@
+package com.smd.project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class notifications : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_notifications)
+    }
+}
